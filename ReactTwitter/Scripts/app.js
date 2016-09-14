03f23e7d97feabd230f349be40d564eff5777e0a
Rafael Romero Lopes <rafarlopes@hotmail.com>
2016-09-13 23:00:26 -0300

    added js dependencies and first version login component

---
+++ ReactTwitter/Scripts/app.js
@@ -0,0 +1,44 @@
+﻿var TwitterApp = React.createClass({
+    handleUserLogin: function(user) {
+        console.log(user);
+    },
+    render: function() {
+        return (
+            <div> 
+                <Login  onLoginSubmit={this.handleUserLogin}/>
+            </div>
+        );
+    }
+});
+
+var Login = React.createClass({
+    getInitialState: function() {
+        return { user: '' };
+    },
+    handleLoginChange: function(e) {
+        this.setState({user: e.target.value});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var user = this.state.user.trim();
+
+        if (!user)
+            return;
+
+        this.props.onLoginSubmit(user);
+        this.setState({user: ''});
+    },
+    render: function() {
+        return (
+            <form className="loginForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Your login" value={this.state.user} onChange={this.handleLoginChange} />
+                <input type="submit" value="Login" />
+            </form>
+        );
+    }
+});
+
+ReactDOM.render(
+    <TwitterApp />,
+    document.getElementById('app')
+);
