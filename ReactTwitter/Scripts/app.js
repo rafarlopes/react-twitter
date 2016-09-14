3a8105c87e9d549e6b4ccddcd45152b2f3e5f2f7
Rafael Romero Lopes <rafarlopes@hotmail.com>
2016-09-14 00:03:50 -0300

    added tweet list and form

--- ReactTwitter/Scripts/app.js
+++ ReactTwitter/Scripts/app.js
@@ -7,7 +7,7 @@
     },
     render: function() {
         var componentToRender = this.state.user 
-            ? <TwitterHome/>
+            ? <TwitterHome user={this.state.user}/>
             : <Login onLoginSubmit={this.handleUserLogin}/>;
 
         return (
@@ -46,11 +46,83 @@ var Login = React.createClass({
 });
 
 var TwitterHome = React.createClass({
-   render: function() {
-       return (
-            <p>Tweets</p>
+    getInitialState: function() {
+        return { tweets: [] , user: this.props.user };
+    },
+    handleTweetSubmit: function(message) {
+        var tweets = this.state.tweets;
+        
+        var tweet = {
+            user: this.state.user,
+            date: Date.now(),
+            message: message
+        };
+        
+        var newTweets = tweets.concat([tweet]);
+        this.setState({ tweets: newTweets, user: this.state.user });
+        
+    },
+    render: function() {
+        return (
+            <div>
+                <TweetForm onTweetSubmit={this.handleTweetSubmit}/>
+                <p>Tweets</p>
+                <TweetList tweets={this.state.tweets}/>
+            </div>
         );
-   }
+    }
+});
+
+var TweetForm = React.createClass({
+    getInitialState: function() {
+        return { tweet: '' };
+    },
+    handleTweetChange: function(e) {
+        this.setState({tweet: e.target.value});
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var tweet = this.state.tweet.trim();
+
+        if (!tweet)
+            return;
+
+        this.props.onTweetSubmit(tweet);
+        this.setState({tweet: ''});
+    },
+    render: function() {
+        return (
+            <form className="tweetForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="What's happening?" value={this.state.tweet} onChange={this.handleTweetChange} />
+                <input type="submit" value="Tweet!" />
+            </form>
+        );
+    }
+});
+
+var TweetList = React.createClass({
+    render: function() {
+        var tweetNodes = this.props.tweets.map(function(tweet) {
+            return (
+              <Tweet user={tweet.user} tweet={tweet.message} />
+            );
+        });
+        return (
+            <div className="tweetList">
+                {tweetNodes}
+            </div>
+        );
+    }
+});
+
+var Tweet = React.createClass({
+    render: function() {
+        return (
+            <div className="tweet">
+                <b>@{this.props.user}:</b> {this.props.tweet}
+            </div>
+        );
+    }
 });
 
 ReactDOM.render(
